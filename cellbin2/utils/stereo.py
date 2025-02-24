73a350bc8b5475dada559389a86f22e99083184f
Zhonghan <dengzhonghan521@gmail.com>
2025-02-24 10:18:25 +0800

    update stereo.py

--- cellbin2/utils/stereo.py
+++ cellbin2/utils/stereo.py
@@ -6,11 +6,11 @@ from cellbin2 import __version__
 
 def generate_stereo_file(
         save_path: pathlib.PosixPath,
-        registered_image: pathlib.PosixPath = "",
-        compressed_image: pathlib.PosixPath = "",
-        matrix_template: pathlib.PosixPath = "",
-        gef: pathlib.PosixPath = "",
-        cellbin_gef: pathlib.PosixPath="",
+        registered_image: pathlib.PosixPath = pathlib.PosixPath(""),
+        compressed_image: pathlib.PosixPath = pathlib.PosixPath(""),
+        matrix_template: pathlib.PosixPath = pathlib.PosixPath(""),
+        gef: pathlib.PosixPath = pathlib.PosixPath(""),
+        cellbin_gef: pathlib.PosixPath = pathlib.PosixPath(""),
         sn="",
 ):
     import json
@@ -69,15 +69,15 @@ def generate_stereo_file(
     else:
         with save_path.open("r") as f:
             stereo_data = json.load(f)
-        if registered_image != "":
+        if registered_image.name != "":
             stereo_data["images"]["registered_image"] = str(registered_image.name)
-        if compressed_image != "":
+        if compressed_image.name != "":
             stereo_data["images"]["compressed_image"] = str(compressed_image.name)
-        if matrix_template != "":
+        if matrix_template.name != "":
             stereo_data["images"]["matrix_template"] = str(matrix_template.name)
-        if gef != "":
+        if gef.name != "":
             stereo_data["StereoMap_explorer_files"]["transcriptomics"]["gef"] = [str(gef.name)]
-        if cellbin_gef != "":
+        if cellbin_gef.name != "":
             stereo_data["StereoMap_explorer_files"]["transcriptomics"]["cellbin_gef"] = [str(cellbin_gef.name)]
     with open(str(save_path), 'w') as fh:
         fh.write(json.dumps(stereo_data, indent=4))
