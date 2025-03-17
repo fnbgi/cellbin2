eabdb823ebf2a095bd9523b3e0970227307f97e1
shican123 <shican2001@163.com>
2025-03-17 11:34:31 +0800

    update xenium related models

--- cellbin2/dnn/segmentor/utils.py
+++ cellbin2/dnn/segmentor/utils.py
@@ -6,11 +6,15 @@ SUPPORTED_MODELS = [
     f'cellseg_bcdu_SHDI_221008_tf',
     f'cellseg_bcdu_H_240823_tf',
     f'cellseg_unet_RNA_20230606',
+    f'cellseg_bcdu_H_20250303_tf_R',
+    f'cellseg_bcdu_D_250225_tf_R',
 ]
 SUPPORTED_STAIN_TYPE_BY_MODEL = {
     SUPPORTED_MODELS[0]: [TechType.ssDNA, TechType.HE, TechType.DAPI],
     SUPPORTED_MODELS[1]: [TechType.HE],
-    SUPPORTED_MODELS[2]: [TechType.Transcriptomics]
+    SUPPORTED_MODELS[2]: [TechType.Transcriptomics],
+    SUPPORTED_MODELS[3]: [TechType.HE],
+    SUPPORTED_MODELS[4]: [TechType.DAPI]
 }
 weight_name_ext = '_weights_path'
 TechToWeightName = {}
