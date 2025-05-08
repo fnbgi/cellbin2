404e6923b3ea40cefaab6f5fe1830d444b01bf47
lizepeng1 <lizepeng1@genomics.cn>
2025-05-08 16:05:30 +0800

    # R&D compatibility changes.

--- cellbin2/utils/stereo_chip.py
+++ cellbin2/utils/stereo_chip.py
@@ -6,6 +6,12 @@ import pandas as pd
 from typing import Tuple
 from enum import Enum
 from cellbin2.utils import clog
+from stereo_chip_name import get_chip_prefix_info
+
+try:
+    from prettytable import PrettyTable
+except ImportError:
+    PrettyTable = None
 
 
 class ChipNameType(Enum):
@@ -26,9 +32,11 @@ POINTS_BEGIN_Y = 105
 POINTS_END_X = 163275
 POINTS_END_Y = 163275
 
+CHIP_TITLE_NAME = ['A', 'B', 'C', 'D', 'Y', 'E', 'F', 'G', 'H', 'Q', 'S']
+
 
 class StereoChip(object):
-    def __init__(self, chip_mask_file: str = ''):
+    def __init__(self, chip_mask_file: str = '', strong_verification: bool = True):
         """ Coordinate reference
                 1   2   3   4   5   6
             G   -   -   -   -   -   -
@@ -56,6 +64,10 @@ class StereoChip(object):
         self.is_from_S13: bool = False  # 拆分自S13还是S6
         self.S1_fov_count = 6.8
         self.exp_r = [0.2, 0.2]  # WH
+
+        self._s13_label = ['Y', 'Q']
+        self._chip_rows = '0_0'
+        self._chip_cols = '0_0'
 
     @staticmethod
     def _create_points(b, e):
@@ -99,11 +111,14 @@ class StereoChip(object):
         track_points_data = self.create_track_points()
         track_points_data = pd.DataFrame(track_points_data, columns = ['x', 'y'])
 
+        _info = None
         if self.is_from_S13:
             if min(self.chip_specif) == 0.5:
                 info = self.chip_mask['T10_90_90_s'][self._name[-4:]]
                 sx, sy = self._get_start_margin(self.chip_mask['T10_90_90_s'])
             else:
+                if max(self.chip_specif) > 1:
+                    _info = self.chip_mask['T10_90_90_230508'][self._name[-4:-2]]
                 info = self.chip_mask['T10_90_90_230508'][self._name[-2:]]
                 sx, sy = self._get_start_margin(self.chip_mask['T10_90_90_230508'])
 
@@ -112,14 +127,18 @@ class StereoChip(object):
                 info = self.chip_mask['T10_41_41_s'][self._name[-4:]]
                 sx, sy = self._get_start_margin(self.chip_mask['T10_41_41_s'])
             else:
+                if max(self.chip_specif) > 1:
+                    _info = self.chip_mask['T10_41_41_230508'][self._name[-4:-2]]
                 info = self.chip_mask['T10_41_41_230508'][self._name[-2:]]
                 sx, sy = self._get_start_margin(self.chip_mask['T10_41_41_230508'])
 
-        self.exp_r = [sum(info['lr_expand']), sum(info['ud_expand'])]
+        self._chip_rows, self._chip_cols, lr, ud = self._get_chip_rc(info, _info)
+        self.exp_r = [round(sum(lr), 1), round(sum(ud), 1)]
+
         first_xy = list(map(lambda x: x * MASK_FOV_LEN, (sx, sy)))
 
-        row = list(map(int, info['fov_row'].split('-')))
-        col = list(map(int, info['fov_col'].split('-')))
+        row = list(map(int, self._chip_rows.split("-")))
+        col = list(map(int, self._chip_cols.split("-")))
 
         mask_x_min = (col[0] - 1) * MASK_FOV_LEN
         mask_x_max = col[1] * MASK_FOV_LEN
@@ -163,6 +182,28 @@ class StereoChip(object):
 
         self._00pt = (zero_x, zero_y)  # 00点距矩阵图左上角坐标
         self._chip_00pt = chip_point  # 00点距芯片左上角坐标
+
+    def _get_chip_rc(self, info1, info2):
+        if max(self.chip_specif) > 1:
+            row_list = list(map(int, info1["fov_row"].split("-"))) + \
+                       list(map(int, info2["fov_row"].split("-")))
+            col_list = list(map(int, info1["fov_col"].split("-"))) + \
+                       list(map(int, info2["fov_col"].split("-")))
+
+            row = "-".join([str(min(row_list)), str(max(row_list))])
+            col = "-".join([str(min(col_list)), str(max(col_list))])
+
+            lr = [info2["lr_expand"][0], info1["lr_expand"][1]]
+            ud = [info2["ud_expand"][1], info1["ud_expand"][0]]
+
+        else:
+            row = info1["fov_row"]
+            col = info1["fov_col"]
+
+            lr = info1["lr_expand"]
+            ud = info1["ud_expand"]
+
+        return row, col, lr, ud
 
     @property
     def zero_zero_point(self, ): return self._00pt
@@ -224,22 +265,32 @@ class StereoChip(object):
 
         title_name = sorted(set([i[0] for i in list(self.chip_mask['T10_90_90_230508'].keys())]))
         name_len = len(self._name)
-        if self.name_type == ChipNameType.SHORT:
-            if name_len == 8:
-                self.chip_specif = [1, 1]
+        try:
+            if self.name_type == ChipNameType.SHORT:
+                if name_len == 8:
+                    self.chip_specif = [1, 1]
+                else:
+                    self.chip_specif = suffix_parser(self._name[-4:], title_name)
+            elif self.name_type == ChipNameType.LONG:
+                suffix = self._name.split('_')[-1]
+                if len(suffix) == 2:
+                    self.chip_specif = [1, 1]
+                else:
+                    self.chip_specif = suffix_parser(suffix, title_name)
             else:
-                self.chip_specif = suffix_parser(self._name[-4:], title_name)
-        elif self.name_type == ChipNameType.LONG:
-            suffix = self._name.split('_')[-1]
-            if len(suffix) == 2:
                 self.chip_specif = [1, 1]
-            else:
-                self.chip_specif = suffix_parser(suffix, title_name)
+        except ValueError:
+            self.chip_specif = [1, 1]
+        except Exception as e:
+            clog.warning(
+                f'{e}, set chip specif to [1, 1]'
+            )
+            self.chip_specif = [1, 1]
 
     def is_after_230508(
             self,
-            s13_min_num=395,
-            s6_min_num=3205,
+            s13_min_num = 395,
+            s6_min_num = 3205,
             deprecated_word = ["B", "I", "O"]
     ) -> bool:
         """  配准前置用该参数作为是否满足调用的条件，这是条件之一，还需要满足拍图时芯片放置角度Rot90=0
@@ -248,43 +299,122 @@ class StereoChip(object):
         :param deprecated_word:
         :return: True表示该芯片产自230508后
         """
-        if self.name_type is ChipNameType.SHORT:
+        try:
+            if self.name_type is ChipNameType.SHORT:
 
-            for _w in deprecated_word:
-                if _w in self.chip_name[-4:]:
-                    return False
+                for _w in deprecated_word:
+                    if _w in self.chip_name[-4:]:
+                        return False
 
-            if self.is_from_S13:
-                if int(self.chip_name[1: 1 + 5]) >= s13_min_num:
-                    return True
+                if self.is_from_S13:
+                    if int(self.chip_name[1: 1 + 5]) >= s13_min_num:
+                        return True
+                    else:
+                        return False
                 else:
-                    return False
-            else:
-                if int(self.chip_name[1: 1 + 5]) >= s6_min_num:
-                    return True
-                else:
-                    return False
-        else:  # 长码现在无法判断，因此算作旧数据
+                    if int(self.chip_name[1: 1 + 5]) >= s6_min_num:
+                        return True
+                    else:
+                        return False
+            else:  # 长码现在无法判断，因此算作旧数据
+                return False
+        except ValueError:
+            return False
+        except Exception as e:
+            clog.warning(f'{e}')
             return False
 
-    def parse_info(self, chip_no: str):
-        self._name = chip_no
-        name_len = len(self._name)  # 长码还是断码判断
+    def _unknown_process(self):
+        clog.warning('Chip name {} lengths is not valid, set chip type to UNKNOWN.'.format(self.chip_name))
+        self.name_type = ChipNameType.UNKNOWN
+
+        self.chip_specif = [1, 1]
+        self.is_from_S13 = False
+        self.exp_r = [0.2, 0.2]
+        self._chip_rows = self._chip_cols = '0_0'
+        self._00pt = [3150, 3150]
+        self._chip_00pt = [0, 0]
+        self.set_chip_size()
+
+        # clog.info('Parse chip info as [SN, NameType, fromS13, Specif, Size(WH)] == ({}, {}, {}, {}, {})'.format(
+        #     self._name, self.name_type, self.is_from_S13, self.get_chip_specif_str(), (self.width, self.height)))
+
+    def _base_rules(self, ):
+        name_len = len(self._name)
+
+        if name_len == 0: return 0
+
+        if self._name[0] not in CHIP_TITLE_NAME: return 0
+
         if name_len in [8, 10]:
+            if not self._name[1: 6].isdigit(): return 0
+            if not 97 <= ord(self._name[6].lower()) <= 122: return 0
+
+            if name_len == 8:
+                if not self._name[7].isdigit(): return 0
             self.name_type = ChipNameType.SHORT
         elif name_len in [14, 16, 17, 18]:
             self.name_type = ChipNameType.LONG
         else:
-            self.name_type = ChipNameType.UNKNOWN
+            return 0
 
-        self.is_from_S13 = self._name[0] == 'Y' and True or False  # 判断S13
+        return 1
+
+    def _print_info(self, ):
+        """
+
+        Returns:
+
+        """
+        if PrettyTable is None:
+            return
+
+        tb = PrettyTable(["Chip Name", "Chip Type", "S6 | S13", "Chip Size",
+                          "Points_00", "Points_chip_00", "Rows", "Cols", "Expand", "Shape"])
+        tb.add_row(
+            [self._name, self.name_type, 'S13' if self.is_from_S13 else 'S6', self.get_chip_specif_str(),
+             self._00pt, self._chip_00pt, self._chip_rows, self._chip_cols, self.exp_r,
+             [self.height, self.width]]
+        )
+        clog.info(f"\n {tb}")
+
+    def parse_info(self, chip_no: str, print_flag = False):
+        """
+        Args:
+            chip_no:
+            print_flag:
+
+        Returns:
+
+        """
+        self._name = chip_no
+
+        # scn = get_chip_prefix_info(self._name[0], self._name)
+
+        key = self._base_rules()
+        if key == 0:
+            self._unknown_process()
+            if print_flag:
+                self._print_info()
+            return
+
+        self.is_from_S13 = self._name[0] in self._s13_label and True or False  # 判断S13
         self.set_chip_specif()
         if self.is_after_230508():
-            self.set_zero_point_info()
+            try:
+                self.set_zero_point_info()
+            except KeyError:
+                self._unknown_process()
+                if print_flag:
+                    self._print_info()
+                return
+
         self.set_chip_size()
 
-        clog.info('Parse chip info as [SN, NameType, fromS13, Specif, Size(WH)] == ({}, {}, {}, {}, {})'.format(
-            self._name, self.name_type, self.is_from_S13, self.get_chip_specif_str(), (self.width, self.height)))
+        if print_flag:
+            self._print_info()
+        # clog.info('Parse chip info as [SN, NameType, fromS13, Specif, Size(WH)] == ({}, {}, {}, {}, {})'.format(
+        #     self._name, self.name_type, self.is_from_S13, self.get_chip_specif_str(), (self.width, self.height)))
 
 
 def main():
@@ -303,25 +433,25 @@ if __name__ == '__main__':
     # main()
     curr_path = os.path.dirname(os.path.realpath(__file__))
     sc = StereoChip(chip_mask_file = os.path.join(curr_path, r'../config/chip_mask.json'))
-    # sc.parse_info(chip_no = 'D05106E2')
-    # print(1)
-    word = 'ACDEFGHJKLMNP'
-    num = '123456789ACDE'
-
-    points_list = []
-
-    for i in range(len(word)):
-        for j in range(len(num)):
-            for k in range(4):
-                sc.parse_info(chip_no='Y03950' + word[i] + num[j] + f'1{k + 1}')
-                # zz = sc.zero_zero_point
-                # print(zz)
-                # print("***************")
-                # print(np.array(sc.zero_zero_point))
-                points_list.append(np.array(sc.zero_zero_chip_point))
-                # print(np.array(sc.zero_zero_chip_point))
-                # print(2940 - np.array(sc.zero_zero_chip_point))
-
+    sc.parse_info(chip_no = 'E40009G6', print_flag=True)
     print(1)
-    np.savetxt(r"G:\temp\S13_small.txt", points_list)
+    # word = 'ACDEFGHJKLMNP'
+    # num = '123456789ACDE'
+    #
+    # points_list = []
+    #
+    # for i in range(len(word)):
+    #     for j in range(len(num)):
+    #         for k in range(4):
+    #             sc.parse_info(chip_no='Y03950' + word[i] + num[j] + f'1{k + 1}')
+    #             # zz = sc.zero_zero_point
+    #             # print(zz)
+    #             # print("***************")
+    #             # print(np.array(sc.zero_zero_point))
+    #             points_list.append(np.array(sc.zero_zero_chip_point))
+    #             # print(np.array(sc.zero_zero_chip_point))
+    #             # print(2940 - np.array(sc.zero_zero_chip_point))
+    #
+    # print(1)
+    # np.savetxt(r"G:\temp\S13_small.txt", points_list)
 
